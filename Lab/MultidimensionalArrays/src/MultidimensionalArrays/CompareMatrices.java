5690df8336ba1f3973cf5be6942e17b1c78a4a9c
valentin-stojanov <valentin_stojanov@abv.bg>
2021-05-31 14:03:46 +0300

    Added lab exercise

--- Lab/MultidimensionalArrays/src/MultidimensionalArrays/CompareMatrices.java
+++ Lab/MultidimensionalArrays/src/MultidimensionalArrays/CompareMatrices.java
@@ -7,25 +7,46 @@ public class CompareMatrices {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int input[] = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+
+        int[] input = readArray(scanner.nextLine());
         int rows = input[0];
         int cols = input[1];
 
-        int[][] matrix = new int[rows][cols];
+        int[][] firstMatrix = new int[rows][cols];
 
         for (int row = 0; row < rows; row++) {
-            matrix[row] = Arrays.stream(scanner.nextLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
+            firstMatrix[row] = readArray(scanner.nextLine());
         }
+
+
+        //Read matrix
 //        for (int row = 0; row < rows; row++) {
 //            for (int col = 0; col < cols; col++) {
 //                matrix[row][col] = scanner.nextInt();
 //            }
 //        }
+//        scanner.nextLine();
 
+        // Print matrix
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+    }
+    public static void printMatrix(int[][] matrix){
+        for (int[] arr : matrix) {
+            for (int element : arr) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[] readArray(String line){
+        return Arrays.stream(line.split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
