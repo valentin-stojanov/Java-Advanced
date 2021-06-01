3700be1b07b9f19948445cf78f819fb9ff7ec502
valentin-stojanov <valentin_stojanov@abv.bg>
2021-06-01 16:13:59 +0300

    Added lab exercise

---
+++ Lab/MultidimensionalArrays/src/MultidimensionalArrays/PrintDiagonalsOfSquareMatrix.java
@@ -0,0 +1,10 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class PrintDiagonalsOfSquareMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
