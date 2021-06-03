8450ba6ce814d53b9eb4d04c217e5a7ac31ae457
valentin-stojanov <valentin_stojanov@abv.bg>
2021-06-03 13:38:23 +0300

    Added exercises (to be modified to work with case: 2 5 1 9 5 Nuke it from orbit )

--- Exercises/MultidimensionalArrays/src/MultidimensionalArrays/Crossfire.java
+++ Exercises/MultidimensionalArrays/src/MultidimensionalArrays/Crossfire.java
@@ -19,17 +19,47 @@ public class Crossfire {
 
         String command = scanner.nextLine();
 
-        while (!command.equals("Nuke it from orbit")){
+        while (!command.equals("Nuke it from orbit")) {
             String[] tokens = command.split("\\s+");
             int row = Integer.parseInt(tokens[0]);
             int col = Integer.parseInt(tokens[1]);
-            int radius = Integer.parseInt(tokens[1]);
+            int radius = Integer.parseInt(tokens[2]);
 
+            if (isInMatrix(row, col, matrix)) {
+                for (int i = Math.max(0, col - radius); i < Math.min(matrix.get(row).size(), col + radius); i++) {
+                    matrix.get(row).set(i, -1);
+                }
+                for (int j = Math.max(0, row - radius); j < Math.min(matrix.size(), row + radius); j++) {
+                    if (col < matrix.get(j).size()) {
+                        matrix.get(j).set(col, -1);
+                    }
+                }
 
+                for (int i = 0; i < matrix.size(); i++) {
+                    matrix.get(i).removeIf(e -> e < 0);
+                }
+                matrix.removeIf(list -> list.isEmpty());
+
+            }
 
 
             command = scanner.nextLine();
         }
+        printMatrix(matrix);
+    }
+
+    private static boolean isInMatrix(int row, int col, List<List<Integer>> matrix) {
+        return (0 <= row && row < matrix.size()) && (0 <= col && col < matrix.get(row).size());
+    }
+
+    private static void printMatrix(List<List<Integer>> matrix) {
+        for (List<Integer> list : matrix) {
+            for (Integer integer : list) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     private static void fillMatrix(List<List<Integer>> matrix, int rows, int cols) {
