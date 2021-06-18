58b2c511dc93220d8c21d902f74d3b00d3720b02
valentin-stojanov <valentin_stojanov@abv.bg>
2021-06-18 14:29:22 +0300

    Added exercise

--- Exercises/FunctionalProgramming/src/FunctionalProgramming/ReverseAndExclude.java
+++ Exercises/FunctionalProgramming/src/FunctionalProgramming/ReverseAndExclude.java
@@ -1,7 +1,30 @@
 package FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class ReverseAndExclude {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(numbers);
+        Predicate<Integer> divisibleByNumber = e -> e % n != 0;
+        numbers
+                .stream()
+                .filter(divisibleByNumber)
+                .map(e -> e + " ")
+                .forEach(System.out::print);
 
     }
 }
